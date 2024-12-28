732d65d507dd4ac68f3fc30d21c2db77eb77cf38
Iris Lee <irislee4git@gmail.com>
2024-12-28 18:59:12 -0500

    all models added

---
+++ SendIt/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SendIt.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+        public string UserTypeId { get; set; }
+    }
+}
